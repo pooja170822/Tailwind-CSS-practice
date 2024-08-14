c7b779d9ced5f8947cbb9fb14be703ed3df83b41
Pooja <85984734+pooja170822@users.noreply.github.com>
2024-08-14 07:29:33 +0530

    Add files via upload

---
+++ Responsive Landing Page/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        playfair: "'Playfair Display', serif",
+        lato: "'Lato', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
